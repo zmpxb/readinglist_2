2917a0c343a9312b9234ff8bf3cf909b658665b2
zmpxb <ambition_xiao@163.com>
2018-07-31 10:14:51 +0800

    SpringBoot MockMVC测试用例-MockMVCWebTests.postbook

--- src/main/java/com/xiaozl/readinglist/ReadingListController.java
+++ src/main/java/com/xiaozl/readinglist/ReadingListController.java
@@ -59,4 +59,6 @@ public class ReadingListController {
 		}
 		return "readingList";
 	}
+
+
 }
--- src/test/java/com/xiaozl/readinglist/MockMVCWebTests.java
+++ src/test/java/com/xiaozl/readinglist/MockMVCWebTests.java
@@ -1,9 +1,15 @@
 package com.xiaozl.readinglist;
 
+import static org.hamcrest.Matchers.*;
+import static org.springframework.http.MediaType.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -11,10 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 
 
@@ -42,6 +45,33 @@ public class MockMVCWebTests {
                 .andExpect(view().name("readingList"))
                 .andExpect(model().attributeExists("books"))
                 .andExpect(model().attribute("books", is(empty())));
+    }
+
+    @Test
+    public void postBook() throws Exception {
+        mockMvc.perform(post("/readingList")
+                .contentType(APPLICATION_FORM_URLENCODED)
+                .param("title", "BOOK TITLE")
+                .param("author", "BOOK AUTHOR")
+                .param("isbn", "1234567890")
+                .param("description", "DESCRIPTION"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/readingList"));
+
+        Book expectedBook = new Book();
+        expectedBook.setId(1L);
+        expectedBook.setReader("craig");
+        expectedBook.setTitle("BOOK TITLE");
+        expectedBook.setAuthor("BOOK AUTHOR");
+        expectedBook.setIsbn("1234567890");
+        expectedBook.setDescription("DESCRIPTION");
+
+        mockMvc.perform(get("/readingList").param("reader","craig"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", hasSize(1)))
+                .andExpect(model().attribute("books", contains(samePropertyValuesAs(expectedBook))));
     }
 
 }
